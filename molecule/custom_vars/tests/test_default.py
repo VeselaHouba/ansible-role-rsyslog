8b0071bd49d2fc26c76e76e58d5ed501b39cac90
Michalek <michalek@m-cloud.cz>
2022-03-11 19:11:26 +0100

    Failover

--- molecule/custom_vars/tests/test_default.py
+++ molecule/custom_vars/tests/test_default.py
@@ -6,13 +6,16 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_config_exists(host):
+def test_config_1(host):
     file = host.file("/etc/rsyslog.d/some-remote-host.conf")
     assert file.exists
     assert file.contains('some-remote-host.tld:514')
     assert file.contains('ActionQueueMaxDiskSpace')
+    assert not file.contains('ActionExecOnlyWhenPreviousIsSuspended')
 
 
-def test_config_contains_setting(host):
-    file = host.file("/etc/rsyslog.d/some-remote-host.conf")
+def test_config_2(host):
+    file = host.file("/etc/rsyslog.d/mutli-remote-hosts.conf")
     assert file.exists
+    assert file.contains('ActionExecOnlyWhenPreviousIsSuspended')
+    assert not file.contains('ActionQueueMaxDiskSpace')
